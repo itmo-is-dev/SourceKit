96f50b52a84ed7b007137f01edae49ff368b21c8
DenChika <eholocation@gmail.com>
2023-06-26 01:51:05 +0300

    implemented iequatable collections analyzer

--- src/analyzers/SourceKit.Analyzers.Collections/Analyzers/DictionaryKeyTypeMustImplementEquatableAnalyzer.cs
+++ src/analyzers/SourceKit.Analyzers.Collections/Analyzers/DictionaryKeyTypeMustImplementEquatableAnalyzer.cs
@@ -1,6 +1,9 @@
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using SourceKit.Extensions;
 
 namespace SourceKit.Analyzers.Collections.Analyzers;
 
@@ -27,5 +30,35 @@ public class DictionaryKeyTypeMustImplementEquatableAnalyzer : DiagnosticAnalyze
     {
         context.EnableConcurrentExecution();
         context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+        context.RegisterSyntaxNodeAction(AnalyzeGeneric , SyntaxKind.GenericName);
+    }
+
+    private string GetRequiredInterface(string keyType)
+    {
+        return $"System.IEquatable<{keyType}>";
+    }
+
+    private void AnalyzeGeneric(SyntaxNodeAnalysisContext context)
+    {
+        var node = context.Node as GenericNameSyntax;
+        if (node!.Identifier.Text != "Dictionary")
+            return;
+
+        var key = node.GetFirstGenericArgument();
+            
+        if (key is OmittedTypeArgumentSyntax)
+            return;
+
+        var keyType = context.GetSymbolFromContext(key) as ITypeSymbol;
+
+        var interfaceName = GetRequiredInterface(keyType!.ToString());
+
+        var iequatable = keyType.GetRequiredImplementedInterfaceByName(interfaceName);
+            
+        if (iequatable is not null)
+            return;
+            
+        var diag = Diagnostic.Create(Descriptor, key.GetLocation());
+        context.ReportDiagnostic(diag);
     }
 }
