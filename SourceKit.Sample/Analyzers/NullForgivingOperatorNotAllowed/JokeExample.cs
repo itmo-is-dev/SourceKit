9b14c81f0cf9be09ad2657e4b1ac23f087a533be
ClwnYeti <crazyyeti@mail.ru>
2023-06-27 00:56:38 +0300

    feat: create testing files for null forgiving analyzer and code fix provider

---
+++ SourceKit.Sample/Analyzers/NullForgivingOperatorNotAllowed/JokeExample.cs
@@ -0,0 +1,52 @@
+﻿namespace SourceKit.Sample.Analyzers.NullForgivingOperatorNotAllowed;
+
+public class JokeExample
+{
+    public string? GetJoke(int number)
+    {
+        switch (number)
+        {
+            case 1:
+                return "Lol";
+            case 2:
+                return "Kek";
+            case 3:
+                return "Trololo";
+            default:
+                return null;
+        }
+    }
+
+    public static JokeExample? GetJokeClass(int number)
+    {
+        if (number == 0)
+        {
+            return new JokeExample();
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public static string GetUpperJoke(int instanceNumber, int jokeNumber)
+    {
+        JokeExample jokeInstance = GetJokeClass(instanceNumber)!;
+        
+        string jokeFromInstance = jokeInstance.GetJoke(jokeNumber)!;
+        
+        var jokeFromSequenceNothingNullable = GetJokeClass(instanceNumber)!.GetJoke(jokeNumber)!;
+        var jokeFromSequenceFirstNullable = GetJokeClass(instanceNumber)?.GetJoke(jokeNumber)!;
+        var jokeFromSequenceSecondNullable = GetJokeClass(instanceNumber)!.GetJoke(jokeNumber);
+        
+        string upperJokeFromSequenceNothingNullable = GetJokeClass(instanceNumber)!.GetJoke(jokeNumber)!.ToUpper();
+        string upperJokeFromSequenceFromInstance = jokeInstance.GetJoke(jokeNumber)!.ToUpper();
+        
+        var upperJokeFirstCringeCompilation =
+            GetJokeClass(instanceNumber)!.GetJoke(jokeNumber)!.ToUpper().ToUpper().ToUpper();
+        var upperJokeSecondCringeCompilation =
+            jokeInstance.GetJoke(jokeNumber)!.ToUpper().ToUpper()!.ToUpper();
+        
+        return upperJokeFirstCringeCompilation;
+    }
+}
