39f1875fe46e4295b82a1fe44128a19b46aec404
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-26 15:39:28 +0300

    feat: realise ConvertListForEachIntoForEachLoopCodeFixProvider

--- src/analyzers/SourceKit.Analyzers.Collections/CodeFixes/ConvertListForEachIntoForEachLoopCodeFixProvider.cs
+++ src/analyzers/SourceKit.Analyzers.Collections/CodeFixes/ConvertListForEachIntoForEachLoopCodeFixProvider.cs
@@ -1,7 +1,11 @@
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using SourceKit.Analyzers.Collections.Analyzers;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace SourceKit.Analyzers.Collections.CodeFixes;
 
@@ -9,15 +13,127 @@ namespace SourceKit.Analyzers.Collections.CodeFixes;
 public class ConvertListForEachIntoForEachLoopCodeFixProvider : CodeFixProvider
 {
     public const string Title = "Convert into foreach loop";
-    
+
     public override ImmutableArray<string> FixableDiagnosticIds { get; } =
         ImmutableArray.Create(ListForEachNotAllowedAnalyzer.DiagnosticId);
-    
+
     public override FixAllProvider GetFixAllProvider()
         => WellKnownFixAllProviders.BatchFixer;
 
-    public override Task RegisterCodeFixesAsync(CodeFixContext context)
+    public override async Task RegisterCodeFixesAsync(CodeFixContext context)
     {
-        return Task.CompletedTask;
+        var root = await context.Document
+            .GetSyntaxRootAsync(context.CancellationToken)
+            .ConfigureAwait(false);
+
+        var diagnostic = context.Diagnostics.First();
+        var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+        var invocationExpression = root?
+            .FindToken(diagnosticSpan.Start)
+            .Parent?.AncestorsAndSelf()
+            .OfType<InvocationExpressionSyntax>()
+            .First();
+
+        if (invocationExpression is null)
+        {
+            return;
+        }
+
+        context.RegisterCodeFix(
+            CodeAction.Create(
+                equivalenceKey: ListForEachNotAllowedAnalyzer.DiagnosticId,
+                title: Title,
+                createChangedDocument: cancellationToken =>
+                    ConvertListForEachIntoLoop(context.Document, invocationExpression, cancellationToken)),
+            diagnostic);
+    }
+
+    private async Task<Document> ConvertListForEachIntoLoop(Document document,
+                                                            InvocationExpressionSyntax invocationExpressionSyntax,
+                                                            CancellationToken cancellationToken)
+    {
+        var parentOfFoundInvocationExpression = invocationExpressionSyntax.Parent;
+
+        if (parentOfFoundInvocationExpression is null)
+        {
+            return document;
+        }
+
+        var root = await document.GetSyntaxRootAsync(cancellationToken);
+
+        if (root is null)
+        {
+            return document;
+        }
+
+        var argumentOfInvocationExpression = invocationExpressionSyntax.ArgumentList.Arguments[0]
+            .Expression;
+
+        var foreachType =
+            IdentifierName(
+                Identifier(
+                    TriviaList(),
+                    SyntaxKind.VarKeyword,
+                    "var",
+                    "var",
+                    TriviaList()));
+        var foreachIdentifier = IdentifierName("element");
+        var foreachExpression = ((MemberAccessExpressionSyntax) invocationExpressionSyntax.Expression).Expression;
+        var foreachStatements = new List<StatementSyntax>();
+
+        switch (argumentOfInvocationExpression)
+        {
+            case SimpleLambdaExpressionSyntax lambdaExpressionSyntax:
+                if (lambdaExpressionSyntax.ExpressionBody is not null)
+                {
+                    foreachIdentifier = IdentifierName(lambdaExpressionSyntax.Parameter.Identifier);
+                    foreachStatements.Add(ExpressionStatement(lambdaExpressionSyntax.ExpressionBody));
+                }
+                else if (lambdaExpressionSyntax.Block is not null)
+                {
+                    var statements = lambdaExpressionSyntax.Block.Statements;
+                    foreachStatements.AddRange(statements);
+                }
+                else
+                {
+                    return document;
+                }
+
+                break;
+            case IdentifierNameSyntax identifierNameSyntax:
+                foreachStatements.Add(
+                    ExpressionStatement(
+                        InvocationExpression(
+                                identifierNameSyntax)
+                            .WithArgumentList(
+                                ArgumentList(
+                                    SingletonSeparatedList(
+                                        Argument(
+                                            foreachIdentifier))))));
+                break;
+            case MemberAccessExpressionSyntax expressionSyntax:
+                foreachStatements.Add(
+                    ExpressionStatement(
+                        InvocationExpression(
+                                expressionSyntax)
+                            .WithArgumentList(
+                                ArgumentList(
+                                    SingletonSeparatedList(
+                                        Argument(
+                                            foreachIdentifier))))));
+                break;
+        }
+
+        var forEachStatement = ForEachStatement(
+                foreachType,
+                foreachIdentifier.Identifier,
+                foreachExpression,
+                Block(foreachStatements))
+            .WithLeadingTrivia(LineFeed);
+
+        var newRoot = root.ReplaceNode(parentOfFoundInvocationExpression, forEachStatement);
+
+        return document.WithSyntaxRoot(newRoot);
     }
 }
