b051d836fba4a6fa50eb8320e20b3d31fe577ebb
ronimizy <ronimizy@outlook.com>
2024-12-15 19:28:25 +0300

    fix: unique constructor file name

--- src/generators/SourceKit.Generators.Grpc/Generators/ProtoMessageConstructorGenerator.cs
+++ src/generators/SourceKit.Generators.Grpc/Generators/ProtoMessageConstructorGenerator.cs
@@ -85,7 +85,7 @@ public class ProtoMessageConstructorGenerator : ISourceGenerator
         {
             var command = new FileBuildingCommand(context, CompilationUnit(), message);
             var compilationUnit = _chain.Process(command);
-            var fileName = GetFileName(message.Type.Name);
+            var fileName = GetFileName(message.Type.GetFullyQualifiedName());
 
             var source = compilationUnit.NormalizeWhitespace(eol: "\n", elasticTrivia: true).ToFullString();
             context.AddSource(fileName, source);
