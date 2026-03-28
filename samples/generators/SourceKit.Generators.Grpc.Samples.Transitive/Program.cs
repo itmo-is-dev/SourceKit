a00d07b6767dca2d1264cf34eb8dc9bba8360645
ronimizy <ronimizy@outlook.com>
2026-03-28 18:34:21 +0300

    feat: transitive grpc alias

---
+++ samples/generators/SourceKit.Generators.Grpc.Samples.Transitive/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+Console.WriteLine("Hello, World!");
+
+var model = new ProtoProtoModel(
+    pageToken: "",
+    values: new[] { "" },
+    pageSize: 0,
+    intValues: new[] { 0 },
+    intOneofValue: null,
+    stringOneofValue: null,
+    notNullStringValue: "",
+    nullIntValue: null,
+    nullStringValue: null,
+    mapValue: new Dictionary<int, string> { [1] = "1" },
+    m: new ProtoProtoModel.Types.InnerMessage(@enum: ProtoProtoModel.Types.InnerEnum.Aboba1));
+
+var emptyModel = new ProtoEmptyMessage();
+
+Console.WriteLine(model);
