87de0d32f2926d33182bf17a849cafaf7f2edc6c
ronimizy <ronimizy@outlook.com>
2026-06-13 18:56:34 +0300

    fix: allow const and static readonly fields

--- src/analyzers/SourceKit.Analyzers.MemberAccessibility/Analyzers/FieldCannotBePublicAnalyzer.cs
+++ src/analyzers/SourceKit.Analyzers.MemberAccessibility/Analyzers/FieldCannotBePublicAnalyzer.cs
@@ -36,15 +36,29 @@ public class FieldCannotBePublicAnalyzer : DiagnosticAnalyzer
     {
         var fieldSyntax = (FieldDeclarationSyntax)context.Node;
 
-        if (fieldSyntax.Modifiers.All(x => x.IsKind(SyntaxKind.PublicKeyword) is false))
-        {
+        bool isPublic = fieldSyntax.Modifiers.Any(token => token.IsKind(SyntaxKind.PublicKeyword));
+
+        if (isPublic is false)
             return;
-        }
+
+        bool isConst = fieldSyntax.Modifiers.Any(token => token.IsKind(SyntaxKind.ConstKeyword));
+
+        if (isConst)
+            return;
+
+        bool isStatic = fieldSyntax.Modifiers.Any(token => token.IsKind(SyntaxKind.StaticKeyword));
+        bool isReadonly = fieldSyntax.Modifiers.Any(token => token.IsKind(SyntaxKind.ReadOnlyKeyword));
+
+        if (isStatic && isReadonly)
+            return;
 
         foreach (VariableDeclaratorSyntax variable in fieldSyntax.Declaration.Variables)
         {
             Location location = variable.GetLocation();
-            var diagnostic = Diagnostic.Create(Descriptor, location, fieldSyntax.Declaration.Type, variable.Identifier.Text);
+            var diagnostic = Diagnostic.Create(Descriptor,
+                location,
+                fieldSyntax.Declaration.Type,
+                variable.Identifier.Text);
 
             context.ReportDiagnostic(diagnostic);
         }
