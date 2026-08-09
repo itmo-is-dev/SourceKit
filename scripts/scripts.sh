14b6b42d2adca5eb29186fd69365642e24053873
ronimizy <ronimizy@outlook.com>
2026-08-10 00:35:58 +0300

    log

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 THIS_FILE_DIRECTORY=$(dirname "$0")
-echo initializing scripts from "$THIS_FILE_DIRECTORY"
+echo initializing scripts, args: "$@"
+echo current directory = "$THIS_FILE_DIRECTORY"
 
 source "$THIS_FILE_DIRECTORY"/solution-scripts.sh
