dee091deabc13d37130b786cf64747f4c2c04333
ronimizy <ronimizy@outlook.com>
2026-08-10 00:44:03 +0300

    log packages

--- scripts/solution-scripts.sh
+++ scripts/solution-scripts.sh
@@ -6,7 +6,6 @@ function remove_from_solution() {
     
     while IFS= read -r file
     do
-      echo removing "$file" from "$solution"
       dotnet solution "$solution" remove "$file"
     done <<< "$(find "$path" -name '*.csproj')"
 }
