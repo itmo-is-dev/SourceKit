d15dca25446cce87daf536444bf50c0d5052615f
ronimizy <ronimizy@outlook.com>
2026-08-10 00:40:26 +0300

    fix: directory

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-THIS_FILE_DIRECTORY=$(dirname "$0")
+THIS_FILE_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")
 
 echo initializing scripts, args: "$0" "$@"
 echo current directory = "$THIS_FILE_DIRECTORY"
-
-realpath "$0"
-echo "${BASH_SOURCE[0]}"
 
 source "$THIS_FILE_DIRECTORY"/solution-scripts.sh
